e1b5057fd0be8960b0fc14d59330a4818ff8cffb
arcmee <kihyun3297@gmail.com>
2022-07-31 00:27:54 +0900

    third-party-setting at index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from 'express'
 import routes from './api/routes'
 import logger from './api/middleware/logger.middleware';
+import compression from 'compression';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(logger);
+app.use(compression());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/api/', routes);
 
 // app.get('/', (req: Request, res: Response) => {
